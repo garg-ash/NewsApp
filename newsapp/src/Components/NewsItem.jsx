a65f8f81c849a21855b30d865d6a63859d59295a
Ashish Garg <152148707+garg-ash@users.noreply.github.com>
2024-08-09 17:54:45 +0530

    Update NewsItem.jsx

--- newsapp/src/Components/NewsItem.jsx
+++ newsapp/src/Components/NewsItem.jsx
@@ -26,7 +26,7 @@ export default class NewsItem extends Component {
     let {title, description, imageUrl, newsUrl} = this.props
     return (
         <div className="card" style={{width: "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <img src={!imageUrl?"https://paultan.org/image/2018/07/Tesla-Fremont-e1531294841669-1200x632.jpg":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
